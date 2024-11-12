3a58ceffdf57c503121a7d3f621e7b5a179b5b26
igowlz <igorarruda112@gmail.com>
2024-11-11 21:03:55 -0300

    Delete Sistema de Gestão de Estoque - Restaurante directory

--- Sistema de Gestão de Estoque - Restaurante/Pedido.py
+++
@@ -1,7 +0,0 @@
-class Pedido:
-    """
-    Essa classe representa o pedido do cliente, o lanche e a quantidade
-    precisamos adicionar aqui um metódo para fazer o desconto dos ingredientes automaticamente
-    """
-    def __init__(self):
-        ...
